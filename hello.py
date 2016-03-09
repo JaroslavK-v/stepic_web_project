f7cb167c1a024dc14087df24b27d3a4f6f542f44
JaroslavK-v <jet_soft@mail.ru>
2016-03-09 23:19:45 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 #возвращает документ с MIME-типом text/plain, содержащий все GET параметры, по одному на каждую строку
 
+bind = "0.0.0.0:8080"
+workers = 3
+user = "nobody"
+
 def application(env, start_response):
 start_response('200 OK', [('Content-Type', 'text/plain')])
 return ["Hello!"]
