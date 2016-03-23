765a7cadcf06fdbb89ccf2d370c9844d839624c4
JaroslavK-v <jet_soft@mail.ru>
2016-03-23 15:26:39 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^signup/.*$','qa.views.test'),
     url(r'^question/(?P<q_id>\d+)/$','qa.views.draw_question', name = 'draw_question'),
     url(r'^ask/.*$','qa.views.test'),
-    url(r'^popular/.*$','qa.views.draw_popular' name= 'draw_popular'),
+    url(r'^popular/.*$','qa.views.draw_popular'),
     url(r'^new/.*$','qa.views.test')
 ]
